a3412751fac0004ba43846f5a97ef81627f6b2fc
Vanushka <30829997+Vanushka@users.noreply.github.com>
2017-11-07 23:43:08 +0300

    another commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react' // импортит пакет react и component с помощью деструктуризации
-//(т.е. Componen - это одно из значений пакета react, в данном случае class)
-import ReactDOM from 'react-dom'  // импортит пакет reactDOM
+import React, { Component } from 'react' // импортит пакет react для работы JSX синтаксиса.
+// Импортируем Component из библиотеки react чтобы использовать его свойства и методы.
+import ReactDOM from 'react-dom'  // импортит пакет reactDOM который нам отрисовывает VirtualDOM для сравнения с реальным домом. И отрисовывает внесённые изменения
 
-class App extends React.Component { // Создали класс App, отнаследовали его от Component
+class App extends React.Component { // Создали класс App, унаследовали его от Component
   render() { // вызываем метод render
-    return( // и в методе render пишем return. Всё что будет записано в return будет отрендерено
-      <div>Hello World!</div> // В данном случаем бы будем рендерить div
+    return( // и в методе render пишем return, который возвратит нам часть кода
+      <div>Hello World!</div> // В данном случаем бы будем возвращать div
     );
   }
 }
 
 ReactDOM.render ( // Вызываем функцию Render и первым аргументом передаём в неё наш компонент App
-  <App />,       // А вторым аргументом мы передаём DOM element в который мы собираемся отрендерить наш компонет App
+  <App />,       // А вторым аргументом мы передаём DOM element в который мы собираемся вернуть наш компонет App
   document.getElementById('root')
 )
-
-// В итоге: Данный код не выполняется браузером, перед этим он транспалится в валидный Java Script
-// с помощью Babel, переводя его в ES6 и только потом выполняется браузером.
-// Мы создали класс, мы заимпортили необходимые библиотеки, мы описали метод render который выводит нам Hello World в браузере.
